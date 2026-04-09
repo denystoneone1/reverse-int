9153e10a0abfdab1b623b7ce3505f757ffcdb1b8
Alex Stone <denystoneone@gmail.com>
2026-04-09 06:14:36 +0500

    complete test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const absoluteValue = Math.abs(n);
+
+  const reversedNumberString = String(absoluteValue)
+    .split('')
+    .reverse()
+    .join('');
+
+  return Number(reversedNumberString);
 };
